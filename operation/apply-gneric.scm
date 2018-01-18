bf92ddc99006347b27ec34efb0f70f4a04c5b419
edent <haoxing.liang@dorabot.com>
2018-01-18 15:19:35 +0800

    corecion

--- operation/apply-gneric.scm
+++ operation/apply-gneric.scm
@@ -18,7 +18,17 @@
          (let ((proc (get op type-tags)))
            (if proc
              (apple proc (map content args))
-             (error " No method")))))
+             (if (= (length args) 2)
+               (let ((type1 (car type-tags))
+                     (type2 (cadr type-tags))
+                     (a1 (car args))
+                     (a2 (cadr args)))
+                 (let (((t1->t2) (get-coercion type1 type2))
+                       ((t2->t1) (get-coercion type2 type1)))
+                   (cond (t1->t2 (apply-generic op (t1->t2 a1) a2))
+                         (t2->t1 (apply-generic op a1 (t2-t1 a2)))
+                         (else (error "no method")))))
+               (error "No method"))))))
 
 (define (add x y) (apply-generic 'add x y))
 (define (sub x y) (apply-generic 'sub x y))
